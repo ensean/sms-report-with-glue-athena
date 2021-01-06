119632dc67c626d4f7aca03b0c8e9bfab12f12ae
liyxzsu <liyxzsu@gmail.com>
2021-01-06 14:25:38 +0800

    update ETL job script

--- parse_phone.py
+++ parse_phone.py
@@ -20,7 +20,11 @@ def parse_country_from_number(phone_number):
     except Exception as e:
         return 'Unknown'
 
-
+def parse_status(deliver_message):
+    if deliver_message.find('been accepted') > 0:
+        return 'Success'
+    else:
+        return 'Fail'
 
 glueContext = GlueContext(SparkContext.getOrCreate())
 
@@ -36,30 +40,33 @@ sms_dur_dyf = glueContext.create_dynamic_frame.from_catalog(database = db_name, 
 
 
 # udf used to convert phone number to country code
-parse_udf = udf(parse_country_from_number, StringType())
+parse_country_udf = udf(parse_country_from_number, StringType())
+parse_status_udf = udf(parse_status, StringType())
+
 sms_dur_df = sms_dur_dyf.toDF()
 
-sms_dur_df = sms_dur_df.withColumn("country_code", parse_udf(sms_dur_df["destinationphonenumber"]))
+sms_dur_df = sms_dur_df.withColumn("country_code", parse_country_udf(sms_dur_df["destinationphonenumber"])).withColumn("status", parse_status_udf(sms_dur_df["deliverystatus"]))
 
 # Turn it back to a dynamic frame
 sms_dur_tmp = DynamicFrame.fromDF(sms_dur_df, glueContext, "nested")
 
 # Rename, cast, and nest with apply_mapping
-sms_dur_nest = sms_dur_tmp.apply_mapping([("publishtimeutc", "string", "publishtimeutc", "string"),
+sms_dur_nest = sms_dur_tmp.apply_mapping([("publishtimeutc", "string", "publish_time_utc", "string"),
                                           ("messageid", "string",
-                                           "messageid", "string"),
+                                           "message_id", "string"),
                                           ("destinationphonenumber", "string",
-                                           "destinationphonenumber", "string"),
+                                           "destination_phone_number", "string"),
                                           ("messagetype", "string",
-                                           "messagetype", "string"),
+                                           "message_type", "string"),
                                           ("deliverystatus", "string",
-                                           "deliverystatus", "string"),
+                                           "delivery_status", "string"),
                                           ("priceinusd", "double",
-                                           "priceinusd", "double"),
+                                           "price_in_usd", "double"),
                                           ("partnumber", "long",
-                                           "partnumber", "long"),
-                                          ("totalparts", "long", "totalparts", "long"),
-                                          ("country_code", "string","country_code", "string")])
+                                           "part_number", "long"),
+                                          ("totalparts", "long", "total_parts", "long"),
+                                          ("country_code", "string","country_code", "string"),
+                                          ("status", "string","status", "string")])
 
 # Write it out in Parquet
 glueContext.write_dynamic_frame.from_options(frame = sms_dur_nest, connection_type = "s3", connection_options = {"path": output_dir}, format = "csv")
